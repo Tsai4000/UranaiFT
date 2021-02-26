8f24da21ee88d4ac6ebccda641bcfc03b2a3e424
Tsai4000 <51948562+Tsai4000@users.noreply.github.com>
2021-02-26 14:53:06 +0800

    Add sql and mongo for test

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ var express = require('express');
 var app = express();
 var port = 5000;
 
+// var sql = require('mysql')
+// var config = {
+//     user: 'root',
+//     password: 'root123',
+//     database: '',
+//     server: 'localhost'
+// }
+// var connection = new sql.createConnection(config, function(err){
+//     if (!err){
+//         console.log('connect')
+//     }else{
+//         console.log(err)
+//     }
+// })
+// connection.connect(function(err){
+//     console.log(err)
+// })
+
+var mongo = require('mongodb').MongoClient
+mongo.connect("mongodb://localhost:27017/uranai", function(err, db){
+    if (err) throw err
+    console.log('connect')
+    db.close
+})
+
 app.get('/', function(req, res){
-    console.log('good')
+    console.log(connection)
 });
 app.get('/test', function(req, res){
     res.send('test ok')
