833f310de098abc86215ac5fabb7e356bce32693
Tsai4000 <51948562+Tsai4000@users.noreply.github.com>
2021-02-28 20:53:11 +0800

    Add insert random api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var TestModel = require('./model/model')
+var SampleModel = require('./model/sample')
 var db = require('./DBConnection')
 var app = express();
 var port = 5000;
@@ -30,6 +31,26 @@ app.get('/test', function(req, res){
     var testEntity = new TestModel({test: 'test1'})
     console.log(testEntity)
 });
+
+app.get('/insert', function(req, res){
+    console.log(req.path, 'call insert')
+    SampleModel.create({
+        lucky: Math.floor(Math.random()*100),
+        wish: Math.floor(Math.random()*100)
+    }, function(err, ent){
+        if(err) console.log(err)
+        console.log(ent, 'inside')
+    })
+    var sampleCollection = db.collection('samples')
+    sampleCollection.find({__v: 0}).toArray(function(err, result){
+        if (err) throw(err)
+        res.send(result)
+    })
+})
+
+app.get('/hiku', function(req, res){
+    
+})
 
 app.listen(port, function(){
     console.log("info",'Server is running at port : ' + port);
