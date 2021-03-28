c78abcf7a23405a5cc7d267a776b59fe4f118346
Tsai4000 <51948562+Tsai4000@users.noreply.github.com>
2021-03-28 20:33:05 +0800

    Make insert api response obj
    Change test verification

--- action/mikuji.js
+++ action/mikuji.js
@@ -1,5 +1,6 @@
 const MikujiModel = require('../model/mikuji')
 const utils = require('../util/util')
+const handleError = require('../error/errorHandle')
 
 exports.insertMikuji = (res, fate) => {
   MikujiModel.aggregate([
@@ -7,15 +8,13 @@ exports.insertMikuji = (res, fate) => {
     { $sample: { size: utils.rand(10) } },
     { $project: { _id: false, __v: false } }
   ]).then(data => {
-    // console.log(data)
     const newKuji = Object.fromEntries(Object.entries(data[0]).map(([key, v]) => {
       return [key, data[utils.rand(data.length)][key]]
     }))
-    // console.log(newKuji)
     MikujiModel.create(newKuji, (err, ent) => {
       if (err) return res.status(400).send(handleError(err))
-      // console.log(ent)
-      res.status(200).send('ok')
+      console.log(newKuji)
+      res.status(200).json(newKuji)
     })
   }).catch(err => {
     console.log(err)
