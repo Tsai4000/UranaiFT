5fd3e8d3b6bbedab2a6ae2792955a62f2cec0ae0
Tsai4000 <51948562+Tsai4000@users.noreply.github.com>
2021-03-28 16:06:59 +0800

    Add canvas for posting pic to server

--- ufe/src/Main.js
+++ ufe/src/Main.js
@@ -1,26 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import hiki from './images/hiki.png'
 import Result from './component/result'
-import  './css/main.css'
+import './css/main.css'
+import { Canvas } from './component/canvas'
+import { useSelector, useDispatch } from 'react-redux';
 
 const style = {
   display: 'flex',
   flex: 1,
-  justifyContent: 'center',
+  flexFlow: 'column',
+  justifyContent: 'start',
   alignItems: 'center'
 }
 
 const Main = () => {
   const [result, setResult] = useState(null)
+  const mikujiResult = useSelector(state => state.mikuji);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setResult(mikujiResult)
+  }, [mikujiResult])
+
   const hiku = () => {
-    fetch('/api/hiku')
-    .then(res => res.json())
-    .then(data => setResult(data))
-    .catch(err => console.log(err))
-  } 
+    fetch('/api/mikuji')
+      .then(res => res.json())
+      .then(data => dispatch({
+        type: "MIKUJI_STORE",
+        payload: data
+      }))
+      .catch(err => console.log(err))
+  }
   return (
     <div style={style}>
-      {!result ? <img src={hiki} onClick={hiku} alt='hiki'/> : <Result result={result}/>}
+      {!result
+        ? <>
+          <img src={hiki} onClick={hiku} alt='hiki' />
+          <Canvas width='300' height='300' api='mikuji' />
+        </>
+        : <Result result={result} />}
+      {/* <Canvas width='300' height='300' api='mikuji' /> */}
     </div>
   )
 }
---
+++ ufe/src/component/canvas.js
@@ -0,0 +1,101 @@
+import React, { useRef, useState, useEffect } from "react"
+import { useDispatch } from 'react-redux';
+
+
+const Canvas = ({ width: w, height: h, api: api }) => {
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setIsDrawing] = useState(false)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    canvas.width = w * 2
+    canvas.height = h * 2
+    canvas.style.width = `${w}px`
+    canvas.style.height = `${h}px`
+
+    const context = canvas.getContext("2d")
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.scale(2, 2)
+    context.lineCap = "round"
+    context.strokeStyle = "white"
+    context.lineWidth = 5
+    contextRef.current = context
+  }, [])
+
+
+  const handleCanvasUpload = async () => {
+    const arrayBuffer = await getCanvasURL();
+    // console.log('arrayBuffer', arrayBuffer);
+
+    const response = await uploadFile(arrayBuffer);
+    // console.log('response', response);
+  }
+
+  const getCanvasURL = () => {
+    // console.log(canvasRef.current.toDataURL("image/png"))
+    return canvasRef.current.toDataURL("image/png")
+  }
+
+  const uploadFile = (arrayBuffer) => {
+    fetch(`${window.origin}/api/${api}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        format: 'png',
+        imgData: arrayBuffer,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      return res.json()
+    }).then(response => {
+      dispatch({
+        type: "MIKUJI_STORE",
+        payload: response
+      })
+    })
+  }
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    setIsDrawing(true)
+  }
+
+  const finishDrawing = () => {
+    contextRef.current.closePath()
+    setIsDrawing(false)
+  }
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return
+    }
+    const { offsetX, offsetY } = nativeEvent
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+  }
+
+  const clearCanvas = () => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext("2d")
+    context.fillStyle = "black"
+    context.fillRect(0, 0, canvas.width, canvas.height)
+  }
+
+  return (<div>
+    <canvas
+      onMouseDown={startDrawing}
+      onMouseUp={finishDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+    />
+    <button onClick={clearCanvas}>reset</button>
+    <button onClick={handleCanvasUpload}>{`POST ${api}`}</button>
+  </div>
+
+  )
+}
+export { Canvas }
